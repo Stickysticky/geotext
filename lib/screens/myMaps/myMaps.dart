f48994a63c6c147475363b7a1032aba05dbed350
Olivier Ettlin <olivier.ettlin@3dathome.fr>
2025-01-13 15:56:43 +0100

    Map started

--- lib/screens/myMaps/myMaps.dart
+++ lib/screens/myMaps/myMaps.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -15,6 +13,42 @@ class MyMaps extends ConsumerWidget {
     return Scaffold(
       backgroundColor: Colors.brown.shade50,
       appBar: CustomAppBar(S.of(context).myMaps),
+      body: Stack( // Utilisez Stack pour empiler des widgets
+        children: [
+          Column(
+            children: [
+              // Ajoutez ici vos autres widgets de contenu
+              Expanded(
+                child: ListView.builder(
+                  itemCount: 10, // Exemple de nombre d'éléments dans la liste
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text('Carte $index'),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+
+          // Le bouton flottant positionné en bas à droite
+          Positioned(
+            bottom: 16, // Distance du bas de l'écran
+            right: 16,  // Distance du bord droit de l'écran
+            child: IconButton(
+              onPressed: () {
+                // Votre action ici
+              },
+              icon: Icon(Icons.plus_one),
+              style: ButtonStyle(
+                backgroundColor: WidgetStateProperty.all(Colors.pink.shade400),
+              ),
+              color: Colors.white,
+              iconSize: 40,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
